96c17b2fd50f2bcfa3064d25363cf076c74edd84
Christian Bahnweg <cebahnweg@gmail.com>
2017-11-06 23:36:07 -0500

    Refactoring.

--- Sword of Dawn/Program.cs
+++ Sword of Dawn/Program.cs
@@ -10,11 +10,9 @@ namespace Sword_of_Dawn
     public class Main
     {
         
-        public static class Game
+        public class Game
         {
           
-            private static void ProfessionSwitch()
-            { }
             static string CharacterName;
             static string AnotherName;
 
@@ -31,23 +29,8 @@ namespace Sword_of_Dawn
             //Asks the player for their character name(s) and saves it/them.
             public static void NameCharacter()
             {
-                Console.WriteLine("What would you like your character's name to be?");
-                CharacterName = Console.ReadLine();
-                Console.WriteLine("Your character's name is now" + " " + CharacterName + ".");
-                Console.WriteLine("\r\n");
-                Console.WriteLine("Is this correct?" + " " + "Y" + "/" + "N");
-                Console.WriteLine("\r\n");
-                //if-then statement for player input on name//
-                ConsoleKeyInfo results;
-                results = Console.ReadKey();
-                if ((results.KeyChar == 'Y') || (results.KeyChar == 'y'))
+                void ProfessionSwitch (string CityName)
                 {
-                    Console.WriteLine("\r\n");
-                    Console.WriteLine("Where were you born?");
-                    Console.WriteLine("Altdorf" + "\r\n" + "Averland" + "\r\n" + "Middenheim" + "\r\n" + "Mordheim" + "\r\n" + "Nuln" + "\r\n");
-
-                    string CityName = Console.ReadLine();
-
                     //Switch case for deciding player's place of birth//
                     switch (CityName)
                     {
@@ -76,7 +59,27 @@ namespace Sword_of_Dawn
                             Console.WriteLine("\r\n");
                             break;
 
-                            }
+                    }
+                }
+                Console.WriteLine("What would you like your character's name to be?");
+                CharacterName = Console.ReadLine();
+                Console.WriteLine("Your character's name is now" + " " + CharacterName + ".");
+                Console.WriteLine("\r\n");
+                Console.WriteLine("Is this correct?" + " " + "Y" + "/" + "N");
+                Console.WriteLine("\r\n");
+                //if-then statement for player input on name//
+                ConsoleKeyInfo results;
+                results = Console.ReadKey();
+                if ((results.KeyChar == 'Y') || (results.KeyChar == 'y'))
+                {
+                    Console.WriteLine("\r\n");
+                    Console.WriteLine("Where were you born?");
+                    Console.WriteLine("Altdorf" + "\r\n" + "Averland" + "\r\n" + "Middenheim" + "\r\n" + "Mordheim" + "\r\n" + "Nuln" + "\r\n");
+
+                    string CityName = Console.ReadLine();
+
+                    ProfessionSwitch(CityName);
+                    
                     //if-else statement for deciding player class. Takes input from Console.ReadLine() and converts it to string, then compares that string against predetermined results.
                     string Profession = Console.ReadLine();
                     if (Profession.Equals("Yes"))
@@ -103,34 +106,7 @@ namespace Sword_of_Dawn
                     string CityName = Console.ReadLine();
                     //Switch case for deciding player's place of birth//
 
-                    switch (CityName)
-                    {
-                        case "Altdorf":
-                            Console.WriteLine("The city of merchants. So tell me: are you a merchant?");
-                            Console.WriteLine("\r\n");
-                            break;
-                        case "Averland":
-                            Console.WriteLine("The city of farmers. So tell me: are you a farmer?");
-                            Console.WriteLine("\r\n");
-                            break;
-                        case "Middenheim":
-                            Console.WriteLine("The city of knights. So tell me: are you a knight?");
-                            Console.WriteLine("\r\n");
-                            Console.ReadLine();
-                            break;
-                        case "Mordheim":
-                            Console.WriteLine("The city of adventurers. So tell me: are you an adventurer?");
-                            Console.WriteLine("\r\n");
-                            break;
-                        case "Nuln":
-                            Console.WriteLine("The city of soldiers. So tell me: are you a soldier?");
-                            Console.WriteLine("\r\n");
-                            break;
-                        default:
-                            Console.WriteLine("That's not a real city! Pick another one.");
-                            Console.WriteLine("\r\n");
-                            break;
-                    }
+                    ProfessionSwitch(CityName);
                     //if-else statement for deciding player class. Takes input from Console.ReadLine().
                     string Profession = Console.ReadLine();
                     if (Profession.Equals("Yes"))
